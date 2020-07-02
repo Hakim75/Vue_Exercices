f1c7a843bd92449ed5c146348c17837d12e8adbc
Hakim75 <cheboub.h@gmail.com>
2020-07-02 18:41:31 +0200

    3rd commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,38 @@
 let vm  = new Vue({
     el: "#app",
     data: {
-        seconds : 0 
+        red: true,
+        green: false,
+        success:true,
+        message:'',
+        firstname: 'Jean',
+        lastname: 'Facteur'
+       
     },
-    mounted: function() {
-        this.$interval = setInterval(() => {
-            console.log("test")
-            this.seconds ++
-        },1000)
-    },
-    destroyed: function(){
-        clearInterval(this.$interval)
-    },
-})
+    computed: { //computed permet de n'utiliser le code que quand la variable a l'intérieur est modifiée au contraire de méthods.
+        cls: function(){
+            console.log("cls called")
+           return this.success === true ? "success" : "error"
+        },
+        testColor: function() {
+            if (this.red) {
+                this.green = true
+                this.red = false
+            } else {
+                this.green = false
+                this.red = true
+            }
+        },
+        fullname: {
+        get: function(){
+            return this.firstname + ' ' + this.lastname
+        },
+        set: function(value){
+            let parts = value.split(" ")
+            this.firstname = parts[0]
+            this.lastname = parts[1]
+            
+        }
+    }
+   
+}})
